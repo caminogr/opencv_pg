7c4b000866d360ad14358432c7b6ab6a8eab9221
kaminora <kaminoura9207@gmail.com>
2019-04-11 01:41:13 +0900

    2019/04/11

---
+++ 2019/04/10.cpp
@@ -0,0 +1,32 @@
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+  cv::Mat src, dst1, dst2;
+
+  // 画像ファイルから画像データを読み込む
+  // * 第2引数に IMREAD_GRAYSCALE を指定すると、グレイスケール画像に変換する
+  src = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+
+  // ヒストグラム平坦化
+  // 画像のコントラストがついて、ハッキリ見えるようになる
+  cv::equalizeHist(src, dst1);
+
+  // ルックアップテーブルを用いたガンマ補正
+  double gamma = 1.5; // ガンマ値
+  // lut: look up table
+  unsigned char lut[256]; // ルックアップテーブル用配列
+  for (int i = 0; i < 256; i++) {
+    lut[i] = pow(i / 255.0, 1 / gamma) * 255.0; // ガンマ補正式
+  }
+  cv::LUT(src, cv::Mat(1, 256, CV_8UC1, lut), dst2); //ルックアップテーブル変換
+
+  // ウィンドウに画像を表示する
+  cv::imshow("原画像", src);
+  cv::imshow("ヒストグラム平坦化", dst1);
+  cv::imshow("ガンマ補正", dst2);
+
+  cv::waitKey();
+
+  return 0;
+}
